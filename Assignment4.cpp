36e14da4aafd196f1c1860683232f681268f054e
Maxence <maxencelamontagne03@gmail.com>
2025-03-17 17:46:44 -0400

    Made the insert and insert helper function

--- Assignment4.cpp
+++ Assignment4.cpp
@@ -11,10 +11,26 @@ private:
         T data;
         TreeNode* left;
         TreeNode* right;
-        TreeNode(const T& value) : data(value), left(nullptr), right(nullptr) {}
+        int amount;
+        //this is the constructor
+        TreeNode(const T& value) : data(value), left(nullptr), right(nullptr), amount(1) {}
     };
     TreeNode* root;
-    void insertHelper(TreeNode*& node, const T& value);
+    void insertHelper(TreeNode*& node, const T& value){
+        if(!node){
+            node = new TreeNode(value);
+        }
+        else if (value < node->data)
+        {
+            insertHelper(node->left,value);
+        }
+        else if (value > node->data){
+            insertHelper(node->data);
+        }
+        else if(value == node->data){
+            ++node->amount;
+        }
+    }
     void preOrderTraversalHelper(TreeNode* node, std::vector<T>& sortedVector);
     void destroyTree(TreeNode* node);
     TreeNode* copyTree(TreeNode* other);
@@ -39,6 +55,20 @@ public:
         
     }
     void insert(const T& value){
+        if(!root){
+            root = value;
+        } 
+        else if (value < root->data)
+        {
+            insertHelper(root->left,value);
+        }
+        else if (value > root->data){
+            insertHelper(root->data);
+        }
+        else if(value == root->data){
+            ++root->amount;
+        }
+        
 
     }
     std::vector<T> preOrderTraversal(){
