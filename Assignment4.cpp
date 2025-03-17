e3d1974b5ec0402fb364309fb5d3f00d6e9cb08a
Maxence <maxencelamontagne03@gmail.com>
2025-03-17 15:53:07 -0400

    Added the Skeleton

--- Assignment4.cpp
+++ Assignment4.cpp
@@ -2,3 +2,29 @@
 
 
 
+template<typename T>
+class BST {
+    
+private:
+    struct TreeNode {
+    T data;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(const T& value) : data(value), left(nullptr), right(nullptr) {}
+    };
+    TreeNode* root;
+    void insertHelper(TreeNode*& node, const T& value);
+    void preOrderTraversalHelper(TreeNode* node, std::vector<T>& sortedVector);
+    void destroyTree(TreeNode* node);
+    TreeNode* copyTree(TreeNode* other);
+
+public:
+    BST(); // Constructor
+    ~BST(); // Destructor
+    BST(const BST& other); // Copy Constructor
+    BST& operator=(const BST& other); // Copy Assignment
+    BST(BST&& other) noexcept; // Move Constructor
+    BST& operator=(BST&& other) noexcept; // Move Assignment
+    void insert(const T& value);
+    std::vector<T> preOrderTraversal();
+};
